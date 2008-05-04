74dd94474e6a64c6eab861f12b92967909eeabb3
martind <martin@dekstop.de>
2008-05-04 21:29:33 +0000

    cope with small/empty tables
    git-svn-id: file:///Users/mongo/data/backups/websites/chilco-textdrive/svn-repo-dump-restored/public/poolradio/trunk@177 cb521945-9b22-0410-a800-9402cadeb437

--- include/db_queries.inc.php
+++ include/db_queries.inc.php
@@ -3,22 +3,35 @@
 # may return less than $num when rows were deleted -- it's a tradeoff for speed
 function query_for_random($num=10) {
     $range_result = mysql_query("SELECT MIN(id) AS min, MAX(id) AS max FROM events");
-    $range_row = mysql_fetch_assoc($range_result); 
-    $ids = array();
-    while (count($ids) < $num) {
-        $ids[] = mt_rand($range_row['min'], $range_row['max']);
-        $ids = array_unique($ids);
+    $range_row = mysql_fetch_assoc($range_result);
+    if ($range_row['max']-$range_row['min'] < $num) {
+        # not enough rows in table -> oldschool way
+        $query = sprintf("SELECT " .
+            "s.name AS source, UNIX_TIMESTAMP(e.created_at) AS date, e.username AS username, e.link AS link, " .
+            "e.radiourl AS radiourl, e.title AS title, e.message AS message, " .
+            "w.link AS wikipedia_link, w.title AS wikipedia_title, w.description AS wikipedia_description " .
+            "FROM events e INNER JOIN sources s ON s.id=e.source_id " .
+            "LEFT OUTER JOIN wikipedia_descriptions w ON e.id=w.event_id " .
+            "ORDER BY RAND() LIMIT %d",
+            mysql_real_escape_string($num));
     }
-    $query = sprintf("SELECT " .
-        "s.name AS source, UNIX_TIMESTAMP(e.created_at) AS date, e.username AS username, e.link AS link, " .
-        "e.radiourl AS radiourl, e.title AS title, e.message AS message, " .
-        "w.link AS wikipedia_link, w.title AS wikipedia_title, w.description AS wikipedia_description " .
-        "FROM events e INNER JOIN sources s ON s.id=e.source_id " .
-        "LEFT OUTER JOIN wikipedia_descriptions w ON e.id=w.event_id " .
-        #"ORDER BY RAND() LIMIT %d",
-        #mysql_real_escape_string($num),
-        "WHERE e.id IN (%s)",
-        implode(', ', $ids));
+    else {
+        $ids = array();
+        while (count($ids) < $num) {
+            $ids[] = mt_rand($range_row['min'], $range_row['max']);
+            $ids = array_unique($ids);
+        }
+        $query = sprintf("SELECT " .
+            "s.name AS source, UNIX_TIMESTAMP(e.created_at) AS date, e.username AS username, e.link AS link, " .
+            "e.radiourl AS radiourl, e.title AS title, e.message AS message, " .
+            "w.link AS wikipedia_link, w.title AS wikipedia_title, w.description AS wikipedia_description " .
+            "FROM events e INNER JOIN sources s ON s.id=e.source_id " .
+            "LEFT OUTER JOIN wikipedia_descriptions w ON e.id=w.event_id " .
+            #"ORDER BY RAND() LIMIT %d",
+            #mysql_real_escape_string($num),
+            "WHERE e.id IN (%s)",
+            implode(', ', $ids));
+    }
     
     return mysql_query($query);
 }
